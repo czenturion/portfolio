e8ffdbd2176284aeae095e66b53040ae784b41ae
czenturion <czenturijon@gmail.com>
2023-03-01 23:05:15 +0300

    v0.1.1 | Small preparations for layout.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import logo from './logo.svg';
 import './styles/App.scss';
 
 const App = () => {
-    return (
-        <div className="App">
-            <div className="app-wrapper">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
+    return <div className="App">
+        <div className="app-wrapper">
+            <header>
+                <div className="header-element"></div>
+            </header>
         </div>
-    );
+    </div>
 }
 
 export default App;
